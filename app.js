45ffa81b695d67d0a6eae2e5aeac53d724866743
b.palowski <brianpalowski00@gmail.com>
2019-02-22 21:15:20 -0800

    Add data

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 // Storage Controller
-const StorageCtrl = (function () {
 
-})();
 
-// Item  Controller
+// Item Controller
 const ItemCtrl = (function () {
 	// Item Constructor
 	const Item = function (id, name, calories) {
@@ -11,74 +9,131 @@ const ItemCtrl = (function () {
 		this.name = name;
 		this.calories = calories;
 	}
-	// Data Structure /  state aka " fake data"
-	const state = {
+
+	// Data Structure / State
+	const data = {
 		items: [
-			{ id: 0, name: 'Steak', calories: 1200 },
-			{ id: 1, name: 'Bread', calories: 200 },
-			{ id: 2, name: 'Egg', calories: 400 }
+			{ id: 0, name: 'Steak Dinner', calories: 1200 },
+			{ id: 1, name: 'Cookie', calories: 400 },
+			{ id: 2, name: 'Eggs', calories: 300 }
 		],
 		currentItem: null,
 		totalCalories: 0
 	}
-	// Public Meth
+
+	// Public methods
 	return {
-		getItemes: function () {
-			return state.items;
+		getItems: function () {
+			return data.items;
 		},
-		logState: function () {
-			return state;
+		addItem: function (name, calories) {
+			let ID;
+			// Create ID
+			if (data.items.length > 0) {
+				ID = data.items[data.items.length - 1].id + 1;
+			} else {
+				ID = 0;
+			}
+
+			// Calories to number
+			calories = parseInt(calories);
+
+			// Create new item
+			newItem = new Item(ID, name, calories);
+
+			// Add to items array
+			data.items.push(newItem);
+
+			return newItem;
+		},
+		logData: function () {
+			return data;
 		}
 	}
 })();
+
+
 
 // UI Controller
 const UICtrl = (function () {
-
 	const UISelectors = {
-		itemList: '#item-list'
+		itemList: '#item-list',
+		addBtn: '.add-btn',
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories'
 	}
 
-	// Public Meth
+	// Public methods
 	return {
 		populateItemList: function (items) {
 			let html = '';
-			items.forEach(function (item) {
-				html += `
-				<li class="collection-item" id="item-${item.id}"><strong>${item.name}:</strong><em>${item.calories}</em>
-				<a href="#" class="secondary-content">
-					<i class="edit-item fa fa-pencil"></i>
-				</a>
-			</li>
-				`
-			});
-			// Insert LI
-			document.querySelector(UISelectors.itemList).innerHTML = html;
 
+			items.forEach(function (item) {
+				html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`;
+			});
+
+			// Insert list items
+			document.querySelector(UISelectors.itemList).innerHTML = html;
+		},
+		getItemInput: function () {
+			return {
+				name: document.querySelector(UISelectors.itemNameInput).value,
+				calories: document.querySelector(UISelectors.itemCaloriesInput).value
+			}
+		},
+		getSelectors: function () {
+			return UISelectors;
 		}
 	}
 })();
 
+
+
 // App Controller
 const App = (function (ItemCtrl, UICtrl) {
+	// Load event listeners
+	const loadEventListeners = function () {
+		// Get UI selectors
+		const UISelectors = UICtrl.getSelectors();
 
+		// Add item event
+		document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+	}
 
-	// Public Meth
+	// Add item submit
+	const itemAddSubmit = function (e) {
+		// Get form input from UI Controller
+		const input = UICtrl.getItemInput();
+
+		// Check for name and calorie input
+		if (input.name !== '' && input.calories !== '') {
+			// Add item
+			const newItem = ItemCtrl.addItem(input.name, input.calories);
+		}
+
+		e.preventDefault();
+	}
+
+	// Public methods
 	return {
 		init: function () {
-			console.log("Loading ...");
-
-			// Items from data structure
-			const items = ItemCtrl.getItemes();
+			// Fetch items from data structure
+			const items = ItemCtrl.getItems();
 
 			// Populate list with items
 			UICtrl.populateItemList(items);
 
-
+			// Load event listeners
+			loadEventListeners();
 		}
 	}
 
-
 })(ItemCtrl, UICtrl);
 
+// Initialize App
 App.init();
